6c87b82a51beea64f71da738d57b0369af8963cb
Liubov P. JD-41 <lovely8419@mail.ru>
2024-09-21 15:26:27 +0100

    hibernate 2

---
+++ src/main/java/ru/Polyaeva/LayerDAO/entities/Customer.java
@@ -0,0 +1,30 @@
+package ru.Polyaeva.LayerDAO.entities;
+
+
+import jakarta.persistence.*;
+
+
+import java.util.List;
+
+@Entity
+@Table(name = "customers")
+@IdClass(String.class)
+public class Customer {
+    @Id
+    @Column
+    public String name;
+
+    @Id
+    @Column
+    public String surname;
+
+    @Id
+    @Column
+    public int age;
+
+    @Column
+    public String phoneNumber;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Order> orders;
+}
---
+++ src/main/java/ru/Polyaeva/LayerDAO/repository/OrderRepository.java
@@ -0,0 +1,27 @@
+package ru.Polyaeva.LayerDAO.repository;
+
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import ru.Polyaeva.LayerDAO.entities.Order;
+
+
+
+@Repository
+public class OrderRepository {
+    private final EntityManager entityManager;
+
+    @Autowired
+    public OrderRepository(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public String getProductName(String name) {
+        String jpql = "SELECT o FROM Order o WHERE o.customer.name = :customerName";
+        return entityManager.createQuery(jpql, Order.class)
+                .setParameter("customerName", name)
+                .getResultList()
+                .getFirst()
+                .productName;
+    }
+}
